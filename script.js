cb3f2e0e9c684f0dcf728ff3f733ed54a50b9ff6
chandruCN <126045844+chandruCN@users.noreply.github.com>
2023-04-08 11:13:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//call the class name from html
+
 var convert = document.querySelector(".convert");
 var fromlocation = document.querySelector(".from");
 var tolocation = document.querySelector(".to");
@@ -12,17 +14,17 @@ var path;
 var endDate;
 
 //############################################################
-
+//function for from location
 fromlocation.addEventListener("change", (event) => {
   start = `${event.target.value}`;
 });
-
+//function for to location
 tolocation.addEventListener("change", (event) => {
   end = `${event.target.value}`;
 });
 
 //################################################################
-
+//dijikstra function for find out the rout distance
 const graph = {
   thirunelveli: { madurai: 2 },
   madurai: { thirunelveli: 2, salem: 3, coimbatore: 3, trichy: 2 },
@@ -33,7 +35,7 @@ const graph = {
   banglore: { salem: 2, coimbatore: 3, mumbai: 3 },
   mumbai: { chennai: 5, banglore: 3 },
 };
-
+//to create the table method
 const printTable = (table) => {
   return Object.keys(table)
     .map((vertex) => {
@@ -44,8 +46,11 @@ const printTable = (table) => {
 };
 
 const tracePath = (table, start, end) => {
+  //empty array for object store
   var path = [];
   var next = end;
+
+  //is for check the given input
   while (true) {
     path.unshift(next);
     if (next === start) {
@@ -56,6 +61,8 @@ const tracePath = (table, start, end) => {
 
   return path;
 };
+
+//give the  key value for arr & obj
 
 const formatGraph = (g) => {
   const tmp = {};
@@ -68,22 +75,27 @@ const formatGraph = (g) => {
   return tmp;
 };
 
+//dijikstra function for find the destination in shortest path
+
 const dijkstra = (graph, start, end) => {
   var map = formatGraph(graph);
-
+  //empty array for value
   var visited = [];
   var unvisited = [start];
   var shortestDistances = { [start]: { vertex: start, distance: 0 } };
 
   var vertex;
+  //while function for find the value from the vertex
   while ((vertex = unvisited.shift())) {
+    console.log(neighbors);
     // Explore unvisited neighbors
     var neighbors = map[vertex].filter((n) => !visited.includes(n.vertex));
-
+    console.log(neighbors);
     // Add neighbors to the unvisited list
     unvisited.push(...neighbors.map((n) => n.vertex));
 
     var distanceToVertex = shortestDistances[vertex].distance;
+    //make a for loop to find the distance from to distance
 
     for (let { vertex: to, distance } of neighbors) {
       var currdistanceToNeighbor =
@@ -100,6 +112,7 @@ const dijkstra = (graph, start, end) => {
 
     visited.push(vertex);
   }
+  //here the process for get the time
 
   const path = tracePath(shortestDistances, start, end);
 
@@ -112,16 +125,18 @@ const dijkstra = (graph, start, end) => {
     .split(" ")
     .join("-");
   startDate = new Date(startDate.replace(/-/g, "/"));
+  //create a empty string to pass the varialble
   var endDate = "",
-    noOfDaysToAdd = shortestDistances[end].distance ,
+    noOfDaysToAdd = shortestDistances[end].distance,
     count = 0;
+  //while loop for calculate the date from the total distance
   while (count < noOfDaysToAdd) {
     endDate = new Date(startDate.setDate(startDate.getDate() + 1));
+    //here the value of 0 is sunday and value 6 is saturday
     if (endDate.getDay() != 0 && endDate.getDay() != 6) {
       count++;
     }
   }
-  console.log(shortestDistances);
 
   var finaldate =
     "YOUR DESTINATION IN SHORTEST PATH :" +
@@ -138,3 +153,4 @@ const dijkstra = (graph, start, end) => {
 convert.addEventListener("click", (event) => {
   dijkstra(graph, start, end);
 });
+f;
